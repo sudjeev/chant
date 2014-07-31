25dbee2adc70497fd226c70c0d67cccaca8d774d
Sudjeev Singh <sudjeev.singh22@gmail.com>
2014-07-30 17:25:10 -0700

    Initial Commit

---
+++ Chant/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Chant
+//
+//  Created by Sudjeev Singh on 7/30/14.
+//  Copyright (c) 2014 SudjeevSingh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
