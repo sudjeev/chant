5510ca7fbc86ffd72ea3cae29d5a0356ef890efd
Sudjeev Singh <sudjeev.singh22@gmail.com>
2014-09-18 13:44:03 -0700

    Added new data model

---
+++ Chant/TeamData.h
@@ -0,0 +1,18 @@
+//
+//  TeamData.h
+//  Chant
+//
+//  Created by Sudjeev Singh on 9/18/14.
+//  Copyright (c) 2014 SudjeevSingh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TeamData : NSObject
+@property (nonatomic, strong) NSString* team;
+@property (nonatomic, strong) NSString* city;
+@property (nonatomic, strong) NSString* abbreviation;
+
+//need a property for the image
+
+@end
