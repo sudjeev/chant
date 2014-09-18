4ad2093c2e6ed8220a46e1256bbaec0d8cb3b122
Sudjeev Singh <sudjeev.singh22@gmail.com>
2014-09-18 11:11:15 -0700

    AddedProfileView

---
+++ Chant/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Chant
+//
+//  Created by Sudjeev Singh on 9/18/14.
+//  Copyright (c) 2014 SudjeevSingh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
