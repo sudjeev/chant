436a070ed47af9c7cf76fafa0247134674c646e2
Sudjeev Singh <sudjeev.singh22@gmail.com>
2015-02-12 02:33:30 -0800

    improved profile UI

--- Chant/ProfileViewController.h
+++ Chant/ProfileViewController.h
@@ -15,14 +15,15 @@
 @property (nonatomic, strong) IBOutlet UILabel* username;
 @property (nonatomic, strong) IBOutlet UILabel* email;
 @property (nonatomic, strong) IBOutlet UILabel* team;
-@property (nonatomic, strong) IBOutlet UIPickerView *picker;
 @property (nonatomic, strong) IBOutlet UIImageView *logo;
 @property (nonatomic, strong) IBOutlet UIButton* change;
-@property (nonatomic, strong) IBOutlet UIButton* save;
 @property (nonatomic, strong) IBOutlet UILabel* upvotes;
+@property (nonatomic, strong) IBOutlet UIView* upvoteView;
+@property (nonatomic, strong) IBOutlet UIView* cellView;
+
+@property (nonatomic, strong) IBOutlet UIView*  logoutView;
 
 - (IBAction)onPasswordReset:(id)sender;
 - (IBAction)onChangeTeam:(id)sender;
-- (IBAction)onSaveTeam:(id)sender;
 
 @end
